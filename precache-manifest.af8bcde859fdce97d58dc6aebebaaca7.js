cae7556be82931e252dee245e7cb79ac0bca7842
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-05-26 13:18:53 -0400

    Updates

---
+++ precache-manifest.af8bcde859fdce97d58dc6aebebaaca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcffbcb57d552227e673758c0a303f2",
+    "url": "/aliens-go-home/index.html"
+  },
+  {
+    "revision": "7e533c3144eb742f8361",
+    "url": "/aliens-go-home/static/css/main.541f02f3.chunk.css"
+  },
+  {
+    "revision": "0ad67c3e4dac94daec33",
+    "url": "/aliens-go-home/static/js/2.02fb2d89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aliens-go-home/static/js/2.02fb2d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e533c3144eb742f8361",
+    "url": "/aliens-go-home/static/js/main.297b5e3a.chunk.js"
+  },
+  {
+    "revision": "017a44ab17f2286025e5",
+    "url": "/aliens-go-home/static/js/runtime-main.ca0d8a37.js"
+  }
+]);
